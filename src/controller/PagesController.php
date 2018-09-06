b56df45c7f20603440a87293320a19942515968b
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-09-06 18:24:20 +0200

    Added PHP validation for contact form

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -25,25 +25,46 @@ class PagesController extends Controller {
         $this->set('currentPage', 'home');
     }
 
-    public function contact()
-    {
+    public function contact() {
+      if(isset($_POST['submit'])) {
+        $this->handleContact();
+      }
         $this->set('title', 'Contact');
         $this->set('currentPage', 'contact');
-        if (isset($_POST['submit'])) {
-            $name = $_POST['name'];
-            $subject = $_POST['subject'];
-            $emailFrom = $_POST['email'];
-            $message = $_POST['message'];
+    }
 
-            $emailTo = "hi@yvesrijckaert.com";
-            $headers = "From: " . $emailFrom;
-            $txt = "You have received an email from " . $name . ".\n\n" . $message;
+    public function handleContact() {
+      $name = $_POST['name'];
+      $subject = $_POST['subject'];
+      $emailFrom = $_POST['email'];
+      $message = $_POST['message'];
 
-            mail($emailTo, $subject, $txt, $headers);
-
-            $_SESSION['info'] = 'Email sent successfully!';
-            header('Location: index.php');
-            exit();
-        }
+      if (empty($name)) {
+        $errors['name'] = 'Please enter your full name.';
+        $_SESSION['error'] = 'Message not send.';
+      }
+      if(empty($emailFrom)) {
+        $errors['email'] = 'Please enter your e-mail.';
+        $_SESSION['error'] = 'Message not send.';
+      } else {
+          if (!filter_var($emailFrom, FILTER_VALIDATE_EMAIL)) {
+              $errors['email'] = 'This is not a correct e-mail.';
+              $_SESSION['error'] = 'Message not send.';
+            }
+      }
+      if(empty($message)) {
+        $errors['message'] = 'Please type your message.';
+        $_SESSION['error'] = 'Message not send.';
+      }
+      if(empty($errors)) {
+        $emailTo = "hi@yvesrijckaert.com";
+        $headers = "From: " . $emailFrom;
+        $txt = "You have received an email from " . $name . ".\n\n" . $message;
+        mail($emailTo, $subject, $txt, $headers);
+        $_SESSION['info'] = 'Email sent successfully!';
+        header('Location: index.php');
+        exit();
+      }
+      $this->set('errors', $errors);
     }
 }
--- src/view/pages/contact.php
+++ src/view/pages/contact.php
@@ -7,23 +7,26 @@
       <div>
         <label for="name">Full name <span class="contact-label-required">&#42;</span></label>
         <p class="form-error error-small"></p>
-        <input class="contact-input" autocomplete="on" type="text" name="name" required />
+        <?php if(!empty($errors['name'])) echo '<p class="form-error error-small php">' . $errors['name'] . '</p>';?>
+        <input class="contact-input" autocomplete="on" type="text" name="name" <?php if(!empty($_POST['name'])) echo 'value="' . $_POST['name'] . '"';?> required />
       </div>
       <div>
         <label for="subject">Subject</label>
         <p class="form-error error-small"></p>
-        <input class="contact-input" type="text" name="subject" />
+        <input class="contact-input" type="text" name="subject" <?php if(!empty($_POST['subject'])) echo 'value="' . $_POST['subject'] . '"';?> />
       </div>
       <div>
         <label for="email">E-mail <span class="contact-label-required">&#42;</span></label>
         <p class="form-error error-small"></p>
-        <input class="contact-input" autocomplete="on" type="email" name="email" required />
+        <?php if(!empty($errors['email'])) echo '<p class="form-error error-small php">' . $errors['email'] . '</p>';?>
+        <input class="contact-input" autocomplete="on" type="email" name="email" <?php if(!empty($_POST['email'])) echo 'value="' . $_POST['email'] . '"';?> required />
       </div>
     </fieldset>
     <fieldset class="contact-fieldset">
       <label for="message">Message <span class="contact-label-required">&#42;</span></label>
       <p class="form-error error-small"></p>
-      <textarea class="contact-input contact-input-text" name="message" cols="30" rows="10" required></textarea>
+      <?php if(!empty($errors['message'])) echo '<p class="form-error error-small php">' . $errors['message'] . '</p>';?>
+      <textarea class="contact-input contact-input-text" name="message" cols="30" rows="10" <?php if(!empty($_POST['message'])) echo 'value="' . $_POST['message'] . '"';?> required></textarea>
       <input class="contact-input contact-input-submit" type="submit" name="submit" value="submit" />
     </fieldset>
   </form>
