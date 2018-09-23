3421874d3e35d110784fca9334826f256732f912
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-09-23 09:11:44 +0200

    loading state added for projects AJAX

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 {
+  // AJAX PROJECTS
+  const $ul = document.querySelector(`.projects-grid`);
+
   const parseCategories = categories => {
-    const $ul = document.querySelector(`.projects-grid`);
     const items = categories
       .map(
         category =>
@@ -8,9 +10,7 @@
             <article class="project">
               <a href="index.php?page=project&id=${category.id}">
                 <h3 class="medium-title project-text">${category.title}</h3>
-                <img class="project-img" src="../assets/img/${
-  category.shorttitle
-}-project.png" />
+                <img class="project-img" src="../assets/img/${category.shorttitle}-project.png" />
               </a>
             </article>
           </li>`
@@ -29,6 +29,22 @@
   };
 
   const handleCategoryChange = e => {
+    $ul.innerHTML = `<div class="loading-page"><svg class="loading-page-spinner" viewBox="0 0 50 50">
+    <path
+      fill="#fff"
+      d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"
+    >
+      <animateTransform
+        attributeType="xml"
+        attributeName="transform"
+        type="rotate"
+        from="0 25 25"
+        to="360 25 25"
+        dur="0.6s"
+        repeatCount="indefinite"
+      />
+    </path>
+  </svg></div>`;
     const category = e.currentTarget.value;
     handleActiveClass(category);
     fetch(`index.php?page=projects&category=${category}`, {
